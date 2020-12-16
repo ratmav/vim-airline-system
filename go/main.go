f92d7151a25e1035808378256905916890b6afe3
Chris Cuming <ratmav@protonmail.com>
2020-12-15 23:55:21 -0500

    sorting it out.

--- go/main.go
+++ go/main.go
@@ -3,11 +3,45 @@ package main
 import (
     "fmt"
 
+    //"github.com/shirou/gopsutil/v3/host"
     "github.com/shirou/gopsutil/v3/mem"
 )
 
-func main() {
-    v, _ := mem.VirtualMemory()
+func date() string {
+	return "TODO"
+}
 
-    fmt.Printf("RAM: %d%%\n", int(v.UsedPercent))
+func cpu() string {
+	return "TODO"
+}
+
+func ram() string {
+	mu, err := mem.VirtualMemory()
+	if err != nil {
+		return "ERR"
+	}
+
+	return fmt.Sprint(int(mu.UsedPercent))
+}
+
+func temp() string {
+	/*
+	dc, err : host.TemperatureStat()
+	if err != nil {
+		return "ERR"
+	}
+
+	return fmt.Sprintf("%s\u00B0C", dc)
+	*/
+	return "WIP"
+}
+
+func main() {
+	fmt.Printf(
+		"%s CPU %s%% RAM %s%% %s\n",
+		date(),
+		cpu(),
+		ram(),
+		temp(),
+	)
 }
